647f64ff25b09636007d1d411c73894e4102c482
KBM007 <maityk007@gmail.com>
2023-03-03 19:20:02 +0530

    endpoints models

---
+++ backend/server/apps/endpoints/urls.py
@@ -0,0 +1,21 @@
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from apps.endpoints.views import EndpointViewSet
+from apps.endpoints.views import MLAlgorithmViewSet
+from apps.endpoints.views import MLAlgorithmStatusViewSet
+from apps.endpoints.views import MLRequestViewSet
+
+
+# Routers provide a way of automatically determining the URL conf
+router = DefaultRouter()
+
+router.register(r"endpoints", EndpointViewSet, basename="endpoints")
+router.register(r"mlalgorithms", MLAlgorithmViewSet, basename="mlalgorithms")
+router.register(r"mlalgorithmstatuses", MLAlgorithmStatusViewSet, basename="mlalgorithmstatuses")
+router.register(r"mlrequests", MLRequestViewSet, basename="mlrequests")
+
+urlpatterns = [
+    path(r"api/v1/", include(router.urls))
+]
